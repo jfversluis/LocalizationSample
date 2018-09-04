5d03e50dd1e9d50534abefcacf696d9f2157da8b
Gerald Versluis <gerald@verslu.is>
2018-09-04 20:49:59 +0200

    Initial commit

---
+++ LocalizationSample/Helpers/TranslateExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalizationSample
+{
+	[ContentProperty("Text")]
+	public class TranslateExtension : IMarkupExtension
+	{
+		private readonly CultureInfo ci = null;
+		private const string ResourceId = "LocalizationSample.Localizations.Language";
+
+		static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(
+			() => new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+		public string Text { get; set; }
+
+		public TranslateExtension()
+		{
+			if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+			{
+				ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+			}
+		}
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (Text == null)
+				return string.Empty;
+
+			var translation = ResMgr.Value.GetString(Text, ci);
+			if (translation == null)
+			{
+#if DEBUG
+				throw new ArgumentException(
+					string.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name));
+#else
+            translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+			}
+			return translation;
+		}
+	}
+}
