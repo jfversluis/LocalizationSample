fdf856b8caa92e2027a158d20fa72e7fff8d63ad
Gerald Versluis <gerald@verslu.is>
2018-09-05 20:21:22 +0200

    Implemented I18N-Portable

--- LocalizationSample/App.xaml.cs
+++ LocalizationSample/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using I18n.net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +18,14 @@ namespace LocalizationSample
 				Localizations.Language.Culture = ci; // set the RESX for resource localization
 				DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
 			}
+
+			I18N.Current
+				.SetNotFoundSymbol("$") // Optional: when a key is not found, it will appear as $key$ (defaults to "$")
+				.SetFallbackLocale("en") // Optional but recommended: locale to load in case the system locale is not supported
+				.SetThrowWhenKeyNotFound(true) // Optional: Throw an exception when keys are not found (recommended only for debugging)
+				.SetLogger(text => Debug.WriteLine(text)) // action to output traces
+				.SetResourcesFolder("I18NPortable") // Optional: The directory containing the resource files (defaults to "Locales")
+				.Init(GetType().GetTypeInfo().Assembly); // assembly where locales live
 
 			InitializeComponent();
 
--- LocalizationSample/MainPage.xaml.cs
+++ LocalizationSample/MainPage.xaml.cs
@@ -1,12 +1,31 @@
-﻿using Xamarin.Forms;
+﻿using System.Globalization;
+using I18n.net;
+using Xamarin.Forms;
 
 namespace LocalizationSample
 {
 	public partial class MainPage : ContentPage
 	{
+		public II18N Strings => I18N.Current;
+
+		public Command SwitchLanguageCommand { get; set; }
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			SwitchLanguageCommand = new Command<string>(SwitchLanguage);
+
+			BindingContext = this;
+		}
+
+		private void SwitchLanguage(string language)
+		{
+			I18N.Current.Locale = language;
+
+			DependencyService.Resolve<ILocalize>().SetLocale(new CultureInfo(language));
+
+			OnPropertyChanged(nameof(Strings));
 		}
 	}
 }
